5db908f0655cdba6ce9161c4bcf0ab23e9e0ef34
Jeremy White <Jeremy@jeremys-mbp-4.lan>
2024-03-02 16:02:12 -0600

    added getById method

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumById)
 
 	router.Run("localhost:8080")
 }
@@ -44,4 +45,16 @@ func postAlbums(c *gin.Context) {
 	}
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
